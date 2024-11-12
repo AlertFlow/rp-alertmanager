fb8c414de130781c024f97912565ffcb15eb9cc0
Justin Neubert <neubert_justin@outlook.com>
2024-11-12 11:51:54 +0100

    Update Alertmanager plugin version to 1.0.6 and refactor Execute method signature

--- plugin.go
+++ plugin.go
@@ -24,7 +24,7 @@ func (p *Alertmanager) Init() models.Plugin {
 	return models.Plugin{
 		Name:    "Alertmanager",
 		Type:    "payload_endpoint",
-		Version: "1.0.5",
+		Version: "1.0.6",
 		Creator: "JustNZ",
 	}
 }
@@ -39,7 +39,11 @@ func (p *Alertmanager) Details() models.PluginDetails {
 	}
 }
 
-func (p *Alertmanager) Execute(context *gin.Context) {
+func (p *Alertmanager) Execute(execution models.Execution, flow models.Flows, payload models.Payload, steps []models.ExecutionSteps, step models.ExecutionSteps, action models.Actions) (data map[string]interface{}, finished bool, canceled bool, no_pattern_match bool, failed bool) {
+	return nil, false, false, false, false
+}
+
+func (p *Alertmanager) Handle(context *gin.Context) {
 	log.Info("Received Alertmanager Payload")
 	incPayload, err := io.ReadAll(context.Request.Body)
 	if err != nil {
